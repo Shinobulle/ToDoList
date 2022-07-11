fe0473e7b45b31fb8f72a18585bfb74c5b43ae46
Antoine VINCIGUERRA <antoine.vinciguerra@nci.fr>
2022-07-11 08:29:40 +0200

    Ajout de pinia

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,29 +1,14 @@
 <template>
   <div class="grid grid-cols-2 gap-4 col-span-3">
-    <input class="
-    col-span-1
-    ml-2
-    rounded-md border-gray-300 border-2 hover:border-gray-500
-    text-center
-    bg-gray-50 hover:bg-white" v-model="name" type="text" placeholder="Ajouter une tâche" @keyup.enter="addTask()" />
-    <input class="
-    col-span-1
-    mr-2
-    rounded-md border-gray-300 border-2 hover:border-gray-500
-    text-center
-    bg-gray-50 hover:bg-white" v-model="search" type="text" placeholder="Rechercher une tâche" @keyup="setSearch()">
+    <input class="input" v-model="name" type="text" placeholder="Ajouter une tâche" @keyup.enter="addTask()" />
+    <input class="input" v-model="search" type="text" placeholder="Rechercher une tâche" @keyup="setSearch()">
     <p class="col-span-2" :class="error != '' ? 'visible': 'invisible' ">{{ error }}</p>
   </div>
   <div class="grid grid-cols-1 gap-4 col-span-3 mb-4 mt-4">
     <ul class="">
       <li v-if="collection.empty()" class="text-lg antialiased font-semibold">Pas de tâche encore défini</li>
       <li v-else v-for="task in collection.filteredItems()" :key="task.id">
-        <div class="mt-3 m-auto
-                    border-2 border-gray-400 rounded-lg hover:border-gray-500
-                    bg-gray-100 hover:bg-gray-50
-                    w-5/6
-                    shadow-md hover:shadow-xl
-                    grid grid-cols-4 gap-2 content-center">
+        <div class="task">
           <Item :item="task" @remove="collection.remove(task)" @toggle:done="task.markAsDone(!task.done)"/>
         </div>
       </li>
