8f1bf52fbdb3ce9cce945a3a8cf362a6a1391840
Antoine VINCIGUERRA <antoine.vinciguerra@nci.fr>
2022-07-11 13:44:35 +0200

    Fix CollectionStore

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="grid grid-cols-2 gap-4 col-span-3">
-    <input class="input" v-model="collectionStore.name" type="text" placeholder="Ajouter une tâche" @keyup.enter="collectionStore.addTask()" />
-    <input class="input" v-model="collectionStore.search" type="text" placeholder="Rechercher une tâche" @keyup="collectionStore.setSearch()">
-    <p class="col-span-2" :class="collectionStore.error != '' ? 'visible': 'invisible' ">{{ collectionStore.error }}</p>
+    <input class="input" v-model="name" type="text" placeholder="Ajouter une tâche" @keyup.enter="addTask()" />
+    <input class="input" v-model="search" type="text" placeholder="Rechercher une tâche" @keyup="setSearch()">
+    <p class="col-span-2" :class="error != '' ? 'visible': 'invisible' ">{{ error }}</p>
   </div>
   <div class="grid grid-cols-1 gap-4 col-span-3 mb-4 mt-4">
     <ul class="">
-      <li v-if="collectionStore.collection.empty()" class="text-lg antialiased font-semibold">Pas de tâche encore défini</li>
-      <li v-else v-for="task in collectionStore.collection.filteredItems()" :key="task.id">
+      <li v-if="collection.empty()" class="text-lg antialiased font-semibold">Pas de tâche encore défini</li>
+      <li v-else v-for="task in collection.filteredItems(search)" :key="task.id">
         <div class="task">
-          <Item :item="task" @remove="collectionStore.collection.remove(task)" @toggle:done="task.markAsDone(!task.done)"/>
+          <Item :item="task" @remove="collection.remove(task)" @toggle:done="task.markAsDone(!task.done)"/>
         </div>
       </li>
     </ul>
@@ -28,10 +28,28 @@ export default {
   },
   data() {
     return {
-      collectionStore: useCollectionStore()
+      collection: useCollectionStore(),
+      error: '',
+      name: '',
+      search: ''
     }
   },
   methods: {
+    addTask(){
+            try{
+              this.collection.create(this.name);
+              this.name ='';
+              this.error='';
+            }catch(e){
+              this.error = "Erreur: "+e.message;
+            }
+        },
+        setSearch(){
+            this.collection.filter(this.search);
+        },
+  },
+  mounted() {
+    this.collection.load();
   }
 }
 </script>
--- src/stores/CollectionStore.js
+++ src/stores/CollectionStore.js
@@ -1,28 +1,59 @@
-import CollectionTask from "@/Collection";
-import StorageLocal from "@/StorageLocal";
+
 import { defineStore } from "pinia";
+import Task from "@/Task";
+import StorageLocal from "@/StorageLocal";
+
 
 export const useCollectionStore = defineStore("CollectionStore", {
     state: () =>{
         return {
-            collection: new CollectionTask((new StorageLocal)),
-            error: '',
-            name: '',
-            search: ''
+            collection: [],
+            filterStr: '',
+            persistenceEngine: new StorageLocal(),
+            persistenceId : 'collection',
         }
     },
     actions:{
-        addTask(){
-            try{
-              this.collection.create(this.name);
-              this.name ='';
-              this.error='';
-            }catch(e){
-              this.error = "Erreur: "+e.message;
-            }
-          },
-          setSearch(){
-            this.collection.filter(this.search);
-          },
+        create(label, isDone = false, id = null){
+            const task = new Task(label, isDone, id );
+            this.add(task);
+        },
+        add(item) {
+            const nextId = this.collection.length;
+            item.setId(nextId);
+            this.collection.push(item);
+            this.save();
+        },
+        sort() {
+            return this.collection.sort((a,b) => a.compare(b));
+        },
+        remove(item) {
+            const indexToRemove = this.findIndex(item);
+            this.collection.splice(indexToRemove, 1);
+            this.save();
+        },
+        findIndex(item) {
+            return this.collection.findIndex((i) => i.getId() === item.getId());
+        },
+        empty() {
+            return this.collection < 1
+        },
+        filter(filterStr) {
+            this.filterStr = filterStr;
+        },
+        filteredItems() {
+            return this.collection.filter(item => item.match(this.filterStr));
+        },
+        save() {
+            this.persistenceEngine.save(this.persistenceId, this.collection);
+        },
+        load() {
+            const rawTasks = this.persistenceEngine.load(this.persistenceId);
+            if (rawTasks == null) return this.collection;
+            this.collection = [];
+            rawTasks.forEach(rawTask => {
+                this.create(rawTask.label, rawTask.done, rawTask.id);
+            });
+        },
     }
 });
